5e77089c0ea2427b7e8d52128378faff8830b13e
hohlov-d <hohlov_d.o@mail.ru>
2024-05-29 22:04:54 +0500

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Dima'
+print('Name:', name, sep='')
+age = 34
+print('Age:', age, sep='')
+age = age + 10
+print('New age:', age, sep='')
+is_student = True
+print('Is Student:', is_student, sep='')
